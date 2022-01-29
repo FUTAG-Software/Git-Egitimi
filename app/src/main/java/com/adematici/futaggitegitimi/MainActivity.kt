ac552c8d20f4e9e3ba0b3a49314af8269207b407
SERAP <ercelserap@gmail.com>
2022-01-29 19:31:39 +0300

    Add fun0

--- app/src/main/java/com/adematici/futaggitegitimi/MainActivity.kt
+++ app/src/main/java/com/adematici/futaggitegitimi/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fun0()
+    }
+
+    fun fun0() {
+        println("fun0")
     }
 }
