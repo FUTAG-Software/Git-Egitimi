5dff3231d60ff67b9bbdea0dac3dc7feedbd3d7a
adematici <adematicis41@gmail.com>
2022-01-29 19:38:39 +0300

    Add fun1

--- app/src/main/java/com/adematici/futaggitegitimi/MainActivity.kt
+++ app/src/main/java/com/adematici/futaggitegitimi/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fun1()
+    }
+
+    fun fun1(){
+        println("fun1")
     }
 }
